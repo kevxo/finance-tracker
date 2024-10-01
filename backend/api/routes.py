a5692b70cb25945cbddb710b112618df14f74e37
Kevin Cuadros <kev040798@gmail.com>
2024-10-01 15:39:50 -0400

    Reformat

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -2,7 +2,9 @@ from fastapi import APIRouter
 
 from api.v1 import user
 from api.v1 import login
+from api.v1 import expense
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="", tags=["users"])
 api_router.include_router(login.router, prefix="", tags=["login"])
+api_router.include_router(expense.router, prefix="", tags=["expenses"])
--- backend/db/schemas/expense.py
+++ backend/db/schemas/expense.py
@@ -2,7 +2,18 @@ from pydantic import BaseModel
 from datetime import date
 
 
-class CreateExpense(BaseModel):
+class UserExpenseCreate(BaseModel):
     amount: float
     category: str
     date: date
+
+
+class ShowUserExpense(BaseModel):
+    amount: float
+    category: str
+    date: date
+    uuid: str
+    user_uuid: str
+
+    class Config:
+        orm_mode = True
