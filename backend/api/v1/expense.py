fa2d3aace0180c817de32ce0a3fda9b9d41152bb
Kevin Cuadros <kev040798@gmail.com>
2024-10-03 14:54:44 -0400

    Reformat

--- backend/api/v1/expense.py
+++ backend/api/v1/expense.py
@@ -1,10 +1,15 @@
 from fastapi import APIRouter, status, HTTPException, Depends
 from sqlalchemy.orm import Session
+from typing import List
 
 from db.schemas.expense import UserExpenseCreate, ShowUserExpense
 from db.db_session import get_db
 from db.models.user import User
-from db.model_helpers.expense import create_new_user_expense
+from db.model_helpers.expense import (
+    create_new_user_expense,
+    list_expenses,
+    get_single_expense,
+)
 
 from api.v1.login import get_current_user
 
@@ -16,15 +21,14 @@ router = APIRouter()
     response_model=ShowUserExpense,
     status_code=status.HTTP_201_CREATED,
 )
-def create_user_expense(
+def create_expense(
     user_uuid: str,
     expense: UserExpenseCreate,
     current_user: User = Depends(get_current_user),
     db: Session = Depends(get_db),
 ):
-    print(current_user, "lookhere")
-    if user_uuid is not current_user.uuid:
-        HTTPException(
+    if user_uuid != current_user.uuid:
+        raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED, detail="User in not authorized."
         )
 
@@ -33,3 +37,42 @@ def create_user_expense(
     )
 
     return user_expense
+
+
+@router.get("/api/v1/user/{user_uuid}/expenses", response_model=List[ShowUserExpense])
+def get_all_expenses(
+    user_uuid: str,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    if user_uuid != current_user.uuid:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="User in not authorized."
+        )
+
+    expenses = list_expenses(db, user_uuid)
+
+    return expenses
+
+
+@router.get("/api/v1/user/{user_uuid}/expenses/{expense_uuid}")
+def get_expense(
+    user_uuid: str,
+    expense_uuid: str,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    if user_uuid != current_user.uuid:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="User in not authorized."
+        )
+
+    expense = get_single_expense(db, user_uuid, expense_uuid)
+
+    if not expense:
+        return HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Expense with uuid {expense_uuid} was not found.",
+        )
+
+    return expense
