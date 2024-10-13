d6afe21dcb981756780b44801348c415d92fc15f
Kevin Cuadros <kev040798@gmail.com>
2024-10-13 18:53:13 -0400

    Create budget POST endpoint with helper method

---
+++ backend/api/v1/budget.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+from sqlalchemy.orm import Session
+from db.schemas.budget import ShowBudget, CreateBudget
+from db.db_session import get_db
+from db.models.user import User
+from db.model_helpers.budget import create_new_budget
+from api.v1.login import get_current_user
+from helpers.authentication import verify_current_user
+
+
+router = APIRouter()
+
+
+@router.post(
+    "/api/v1/users/{user_uuid}/budgets",
+    response_model=ShowBudget,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_budget(
+    user_uuid: str,
+    budget: CreateBudget,
+    db: Session = Depends(get_db),
+    current_user: User = Depends(get_current_user),
+):
+    verify_current_user(user_uuid, current_user.uuid)
+
+    budget = create_new_budget(budget, db, current_user)
+
+    return budget
---
+++ backend/db/model_helpers/budget.py
@@ -0,0 +1,16 @@
+import uuid
+
+from sqlalchemy.orm import Session
+from db.schemas.budget import CreateBudget
+from db.models.user import User
+from db.models.budget import Budget
+
+
+def create_new_budget(budget: CreateBudget, db: Session, user: User):
+    budget = Budget(uuid=str(uuid.uuid4()), user_uuid=user.uuid, **budget.model_dump())
+
+    db.add(budget)
+    db.commit()
+    db.refresh(budget)
+
+    return budget
