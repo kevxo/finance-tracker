3e2005e5d6b4369e85c6ce57fe035bacf92a60f5
Kevin Cuadros <kev040798@gmail.com>
2024-10-13 18:52:11 -0400

    Add new budget route and schema

--- backend/api/routes.py
+++ backend/api/routes.py
@@ -3,8 +3,10 @@ from fastapi import APIRouter
 from api.v1 import user
 from api.v1 import login
 from api.v1 import expense
+from api.v1 import budget
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="", tags=["users"])
 api_router.include_router(login.router, prefix="", tags=["login"])
 api_router.include_router(expense.router, prefix="", tags=["expenses"])
+api_router.include_router(budget.router, prefix="", tags=["budgets"])
--- backend/db/schemas/budget.py
+++ backend/db/schemas/budget.py
@@ -1,7 +1,13 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, UUID4
 from datetime import date
 
 
 class CreateBudget(BaseModel):
+    budget_amount: float
+    month: date
+
+
+class ShowBudget(BaseModel):
+    uuid: UUID4
     budget_amount: float
     month: date
