b503dc8efedd6de47b7dd7df393f72fe32fb0ef2
Kevin Cuadros <kev040798@gmail.com>
2024-11-18 17:47:01 -0500

    Allow all origins

--- backend/main.py
+++ backend/main.py
@@ -24,17 +24,9 @@ def start_services():
 
 app = start_services()
 
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    f"{settings.PROD_URL}"
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
