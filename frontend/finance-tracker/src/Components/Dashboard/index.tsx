4346c314b3b0a173da80f39b4e46efdd9824860f
Kevin Cuadros <kev040798@gmail.com>
2024-11-21 12:31:55 -0500

    Add new Create Expense button

--- frontend/finance-tracker/src/Components/Dashboard/index.tsx
+++ frontend/finance-tracker/src/Components/Dashboard/index.tsx
@@ -3,10 +3,12 @@ import { useEffect, useState } from 'react';
 
 
 import { getUserExpenses } from "../../Services/APIs/Expenses";
-import { Expense } from '../../Types'
+import { Expense, NewExpense } from '../../Types'
+import { ExpenseModal } from "../ExpenseModal";
 
 export function Dashboard() {
-    const [expenses, setExpenses] = useState<[Expense] | []>([])
+    const [expenses, setExpenses] = useState<(Expense | NewExpense)[]>([])
+    const [openModal, setOpenModal] = useState<boolean>(false)
     const token = localStorage.getItem('token')
     useEffect(() => {
         const userExpenses = async () => {
@@ -19,11 +21,20 @@ export function Dashboard() {
         userExpenses()
     }, [token])
 
+    const handleAddExpense = (newExpense: NewExpense) => {
+        setExpenses((prevExpenses) => [...prevExpenses, newExpense]);
+    }
+
     return (
         <div className="overflow-x-auto">
-            <h1 className="text-xl">Expenses</h1>
-            <Button className="" size="sm">Create Expense</Button>
-            <Table>
+            <div className="flex items-center justify-between mt-6">
+                <h1 className="text-xl">
+                    Expenses
+                </h1>
+                <Button className="mr-4" onClick={() => setOpenModal(true)} size="xs">Create Expense</Button>
+            </div>
+            <ExpenseModal isOpen={openModal} handleOnClose={() => setOpenModal(false)} onAddExpense={handleAddExpense}/>
+            <Table className="">
                 <TableHead>
                 <TableHeadCell>Category</TableHeadCell>
                 <TableHeadCell>Amount</TableHeadCell>
