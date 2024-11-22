df9087b2bbe00e4811b1606ba09a36e5b728adea
Kevin Cuadros <kev040798@gmail.com>
2024-11-22 15:01:46 -0500

    Finish testing the new modal feature with button

--- frontend/finance-tracker/src/Components/Dashboard/index.test.tsx
+++ frontend/finance-tracker/src/Components/Dashboard/index.test.tsx
@@ -1,8 +1,8 @@
-import { render, screen, waitFor} from '@testing-library/react'
+import { fireEvent, render, screen, waitFor, within} from '@testing-library/react'
 import '@testing-library/jest-dom';
 
 import { Dashboard } from './index'
-import { getUserExpenses } from '../../Services/APIs/Expenses'
+import { getUserExpenses, createUserExpense } from '../../Services/APIs/Expenses'
 
 jest.mock('../../Services/APIs/Expenses')
 jest.mock('../../env', () => ({
@@ -66,5 +66,104 @@ describe('Dashboard', () => {
             expect(screen.getByText('$200.00')).toBeInTheDocument();
             expect(screen.getByText('2024-10-10')).toBeInTheDocument();
         })
+    })
+
+    it('should click create expense', async () => {
+        render(<Dashboard />)
+
+        await waitFor(async () => {
+            const createExpenseBttn = screen.getByRole('button', {
+                name: /create expense/i
+            })
+
+            expect(createExpenseBttn).toBeVisible()
+
+            await fireEvent.click(createExpenseBttn)
+
+            expect(screen.getByRole('heading', {
+                name: /create new expense/i
+            })).toBeVisible()
+
+            const dialog = screen.getByRole('dialog');
+
+            expect(within(dialog).getByText(/category/i)).toBeVisible()
+            expect(within(dialog).getByText(/amount/i)).toBeVisible()
+            expect(within(dialog).getByText(/date/i)).toBeVisible()
+
+            expect(screen.getByRole('button', {
+                name: /submit/i
+            })).toBeDisabled()
+        })
+    })
+
+    it('should be able to close modal', async () => {
+        render(<Dashboard />)
+
+        await waitFor(async () => {
+            const createExpenseBttn = screen.getByRole('button', {
+                name: /create expense/i
+            })
+
+            expect(createExpenseBttn).toBeVisible()
+
+            await fireEvent.click(createExpenseBttn)
+
+            const header = screen.getByRole('heading', {
+                name: /create new expense/i
+            })
+
+            await fireEvent.click(screen.getByRole('button', {
+                name: /close/i
+            }))
+
+            expect(header).not.toBeVisible()
+        })
+    })
+
+    it('should be able to create expense', async () => {
+        (getUserExpenses as jest.Mock).mockResolvedValueOnce([]);
+
+        (createUserExpense as jest.Mock).mockResolvedValueOnce({
+            amount: 120,
+            category: 'Internet',
+            date: new Date(),
+            uuid: 'TESTUUID'
+        });
+
+        render(<Dashboard />)
+
+        await waitFor(async () => {
+            const createExpenseBttn = screen.getByRole('button', {
+                name: /create expense/i
+            })
+
+            expect(createExpenseBttn).toBeVisible()
+
+            await fireEvent.click(createExpenseBttn)
+
+            const category = screen.getByRole('textbox', {
+                name: /category \*/i
+            })
+            const amount = screen.getByRole('spinbutton', {
+                name: /amount \*/i
+            })
+
+            await fireEvent.change(category, {target: {value: 'Internet'}})
+            await fireEvent.change(amount, {target: {value: '120'}})
+
+            expect(category).toHaveValue('Internet')
+
+            expect(amount).toHaveValue(120)
+
+            const submitButton = screen.getByRole('button', {
+                name: /submit/i
+            })
+
+            await fireEvent.click(submitButton)
+        })
+
+
+       expect(screen.getByText("Internet")).toBeVisible()
+       expect(screen.getByText("$120.00")).toBeVisible()
     })
 })
