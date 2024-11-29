8ad6decda0b40f624e6dda6777f53ce693ea1873
Kevin Cuadros <kev040798@gmail.com>
2024-11-29 12:57:57 -0500

    Add the API call to delete user expenses

--- frontend/finance-tracker/src/Services/APIs/Expenses.tsx
+++ frontend/finance-tracker/src/Services/APIs/Expenses.tsx
@@ -1,11 +1,16 @@
 import { jwtDecode } from "jwt-decode";
 
 import { URI } from '../../env'
+import { Expense } from "../../Types";
 
 interface ExpenseCreateBody {
     amount: number;
     category: string;
     date: string;
+}
+
+interface DeleteExpensesBody {
+    uuids: string[];
 }
 
 export const getUserExpenses = async (token: string) => {
@@ -57,6 +62,35 @@ export const createUserExpense = async (token: string, payload: ExpenseCreateBod
         const data = await response.json();
 
         return data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export const deleteUserExpense = async (token: string, uuids: (Expense["uuid"])[]) => {
+    const decode = jwtDecode(token);
+    const userUuid = decode?.uuid;
+
+    const url: string = `${URI}/api/v1/users/${userUuid}/expenses`
+
+    const payload: DeleteExpensesBody = {
+        uuids: uuids
+    }
+
+    try {
+        const response = await fetch(url, {
+            method: 'DELETE',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
     } catch (err) {
         console.error(err);
     }
