e67f07f31ee249fbfc42e9f3daecc9cccceb6fec
Kevin Cuadros <kev040798@gmail.com>
2024-11-30 21:30:07 -0500

    Add patch call for expense

--- frontend/finance-tracker/src/Services/APIs/Expenses.tsx
+++ frontend/finance-tracker/src/Services/APIs/Expenses.tsx
@@ -13,6 +13,12 @@ interface DeleteExpensesBody {
     uuids: string[];
 }
 
+export interface ExpenseUpdateBody {
+    amount?: number;
+    category?: string;
+    date?: string;
+}
+
 export const getUserExpenses = async (token: string) => {
     const decode = jwtDecode(token);
     const userUuid = decode?.uuid;
@@ -48,6 +54,34 @@ export const createUserExpense = async (token: string, payload: ExpenseCreateBod
     try {
         const response = await fetch(url, {
             method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+export const updateUserExpense = async (token: string, payload: ExpenseUpdateBody, expenseUuid: string) => {
+    const decode = jwtDecode(token);
+    const userUuid = decode?.uuid;
+
+    const url: string = `${URI}/api/v1/users/${userUuid}/expenses/${expenseUuid}`
+
+    try {
+        const response = await fetch(url, {
+            method: 'PATCH',
             headers: {
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json'
