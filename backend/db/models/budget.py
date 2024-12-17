6ec39fea48908e345a64e76419b56245a8ad9019
Kevin Cuadros <kev040798@gmail.com>
2024-12-17 13:06:37 -0500

    Add created_at column and index to user_uuid and month

--- backend/db/models/budget.py
+++ backend/db/models/budget.py
@@ -1,5 +1,6 @@
-from sqlalchemy import String, Column, ForeignKey, Float, Date
+from sqlalchemy import String, Column, ForeignKey, Float, Date, DateTime, Index
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from db.base_class import Base
 
 
@@ -8,5 +9,10 @@ class Budget(Base):
     user_uuid = Column(String, ForeignKey("user.uuid"))
     budget_amount = Column(Float, nullable=False)
     month = Column(Date, nullable=False)
+    created_at = Column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+
+    __table_args__ = (Index("idx_user_uuid_month", "user_uuid", "month"),)
 
     owner = relationship("User", back_populates="budgets")
