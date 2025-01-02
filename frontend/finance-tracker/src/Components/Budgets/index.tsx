e47d0b576fb06c2fdff23d05b3ecc82a224149f6
Kevin Cuadros <kev040798@gmail.com>
2025-01-01 21:26:37 -0500

    Create Budgets componenet WIP

--- frontend/finance-tracker/src/Components/Budgets/index.tsx
+++ frontend/finance-tracker/src/Components/Budgets/index.tsx
@@ -1,20 +1,13 @@
-import { Card, List } from "flowbite-react";
+import { Card, List, Button } from "flowbite-react";
 import { useEffect, useState } from 'react'
 
 import { getCurrentBudget } from "../../Services/APIs/Budgets";
-
-interface Budget {
-    uuid: string;
-    budget_amount: number;
-    month: string;
-    expenses_total: number;
-    remaining_budget: number;
-}
-
+import { Budget } from '../../Types'
+import { BudgetsModal } from "../BudgetsModal";
 export function Budgets() {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const [budgetUuid, setBudgetUuid] = useState<Budget["uuid"]>()
     const [currentBudget, setCurrentBudget] = useState<Budget>()
+    const [budgetUuid, setBudgetUuid] = useState<Budget["uuid"]>(currentBudget?.uuid || "");
+    const [openModal, setOpenModal] = useState<boolean>(false);
     const token = localStorage.getItem('token');
 
     useEffect(() => {
@@ -28,8 +21,13 @@ export function Budgets() {
         retrieveCurrentBudget();
     }, [budgetUuid, currentBudget, token])
 
+    const handleNewBudget = (newBudget: Budget) => {
+        setBudgetUuid(newBudget.uuid);
+    }
+
     return (
         <Card href="#" className="max-w-sm">
+            <BudgetsModal isOpen={openModal} handleOnClose={() => setOpenModal(false)} onNewBudget={handleNewBudget}/>
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 Current Budget
             </h5>
@@ -47,6 +45,17 @@ export function Budgets() {
                     Remaining Budget: { currentBudget?.remaining_budget || '-' }
                 </List.Item>
             </List>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-2">
+                <Button onClick={() => setOpenModal(true)} className="w-full py-1 px-2 text-xs font-medium text-white bg-blue-600 rounded-md hover:bg-blue-700">
+                    New Budget
+                </Button>
+                <Button disabled className="w-full py-1 px-2 text-xs font-medium text-white bg-yellow-500 rounded-md hover:bg-yellow-600">
+                    Edit Budget
+                </Button>
+                <Button disabled className="w-full py-1 px-2 text-xs font-medium text-white bg-red-600 rounded-md hover:bg-red-700">
+                    Remove Budget
+                </Button>
+            </div>
         </Card>
     );
 }
