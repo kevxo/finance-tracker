7edc7d288ec36a71963fe91be82687e118cc9db7
Kevin Cuadros <kev040798@gmail.com>
2025-01-01 21:24:47 -0500

    Add the API call service to createNewBudget

--- frontend/finance-tracker/src/Services/APIs/Budgets.tsx
+++ frontend/finance-tracker/src/Services/APIs/Budgets.tsx
@@ -2,6 +2,11 @@ import { jwtDecode } from "jwt-decode";
 
 import { URI } from '../../env'
 
+interface BudgetCreate {
+    budget_amount: number;
+    month: string;
+}
+
 export const getCurrentBudget = async (token: string, budgetUuid: string) => {
     const decode = jwtDecode(token);
     const userUuid = decode?.uuid;
@@ -14,6 +19,34 @@ export const getCurrentBudget = async (token: string, budgetUuid: string) => {
                 'Authorization': `Bearer ${token}`,
                 'Content-Type': 'application/json'
             }
+        })
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        return data
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+export const createNewBudget = async (token: string, budgetPayload: BudgetCreate) => {
+    const decode = jwtDecode(token);
+    const userUuid = decode?.uuid;
+
+    const url: string = `${URI}/api/v1/users/${userUuid}/budgets`
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(budgetPayload),
         })
 
         if (!response.ok) {
