263c861d71e0b837f7799bd8fcb9fb4e09cae7dd
Kevin Cuadros <kev040798@gmail.com>
2025-01-14 14:23:51 -0500

    Add Patch call to update budget

--- frontend/finance-tracker/src/Services/APIs/Budgets.tsx
+++ frontend/finance-tracker/src/Services/APIs/Budgets.tsx
@@ -1,10 +1,15 @@
 import { jwtDecode } from "jwt-decode";
 
 import { URI } from '../../env'
+import { Budget } from "../../Types";
 
 interface BudgetCreate {
     budget_amount: number;
     month: string;
+}
+
+export interface BudgetUpdateBody {
+    budget_amount?: number;
 }
 
 export const getCurrentBudget = async (token: string, budgetUuid: string) => {
@@ -47,6 +52,35 @@ export const createNewBudget = async (token: string, budgetPayload: BudgetCreate
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(budgetPayload),
+        })
+
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`)
+        }
+
+        const data = await response.json()
+
+        return data
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+export const updateUserBudget = async (token: string, payload: BudgetUpdateBody, budgetUuid: Budget["uuid"]) => {
+    const decode = jwtDecode(token);
+    const userUuid = decode?.uuid;
+
+    const url: string = `${URI}/api/v1/users/${userUuid}/budgets/${budgetUuid}`
+
+
+    try {
+        const response = await fetch(url, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
         })
 
         if (!response.ok) {
